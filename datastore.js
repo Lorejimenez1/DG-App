d4cbe73691e281685481983541a4dd79143945c3
Lorenzo Jimenez <lorenzojimenez101@yahoo.com>
2018-06-06 10:38:52 -0700

    implement app

---
+++ datastore.js
@@ -0,0 +1,103 @@
+const store = [
+  {
+    question:'Besides being an actor what other proffesions has Donald done in his career?',
+    answers: [
+      'Writer',
+      'Basketball player',
+      'Programmer',
+      'Ac repair'
+      ],
+    correctAnswer:'Writer',
+  },
+  {
+    question:'Donald glover had a net Worth of how many million?',
+    answers:[
+      '1 Million',
+      '7 Million',
+      '12 Million',
+      '22 Million'
+      ],
+    correctAnswer:'12 Million',
+  },
+  {
+    question:'Before acting in the show Community what tv show did Donald write for?',
+    answers:[
+      'The Voice',
+      '30 Rock',
+      'The Blacklist',
+      'This Is Us'
+      ],
+    correctAnswer:'30 Rock',
+  },
+  {
+    question:'Where is Donald Glover from?',
+    answers:[
+      'Atlanta,GA',
+      'Stone Mountain,GA',
+      'Nashville,TN',
+      'New York,NY'
+      ],
+    correctAnswer:'Stone Moutaine,GA',
+  },
+  {
+    question:'Where does the rap name "Childish Gambino" come from?', 
+    answers:[
+      'Childhood nickname',
+      'Donalds stand up comedy career',
+      'Wu-tang name generator ',
+      'Family name'
+    ],
+    correctAnswer:'Wu-tang name generator '
+  },
+  {
+    question:'What role did Glover play in the 2017 film Solo: AStar Wars movie?', 
+    answers:[
+      'Hans Solo',
+      'Lando Calrissian',
+      'Beckett',
+      'Chewbacca'
+    ],
+    correctAnswer:'Lando Calrissian'
+  },
+  {
+    question:'Which of these albums was the most recent for Donald Glover?',
+    answers:[
+      'Camp',
+      'Because of the Internet',
+      'Awaken My Love!',
+      'Royalty'
+    ],
+    correctAnswer:'Awaken My Love!'
+  },
+  {
+    question:'In high school he was voted most likely to write for what show?',
+    answers:[
+      'The Simpsons',
+      'Family Guy',
+      'South Park',
+      'Futurama'
+    ],
+    correctAnswer:'The Simpsons'
+  },
+  {
+    question:' How many  youtube views did the song "This is America" recieve in its first week',
+    answers:[
+      '200 Million',
+      '150 Million',
+      '1 Million',
+      '800 thousand'
+    ],
+    correctAnswer:'150 Million'
+  }, 
+  {
+    question:'What does the Gambino music video "This Is America" Depict',
+    answers:[
+      'The american dream',
+      'American culture of dance and hip hop',
+      'Gun violence and mass killings',
+      'Terrorist issues in America'
+    ],
+    correctAnswer:'Gun violence and mass killings'
+  },     
+
+  ];
---
+++ index.js
@@ -0,0 +1,148 @@
+let questionNumber = 0;
+let score = 0;
+
+function generateQuestion() {
+  if (questionNumber < store.length) {
+    return `<div class="question-container">
+    <h2>${store[questionNumber].question}</h2>
+    <form>
+    <fieldset><legend>Select an answer from the options below:</legend>
+    <label class="answer-option">
+    <input type="radio" value="${store[questionNumber].answers[0]}" name="answer" required>
+    <span>${store[questionNumber].answers[0]}</span>
+    </label>
+    <label class="answer-option">
+    <input type="radio" value="${store[questionNumber].answers[1]}" name="answer" required>
+    <span>${store[questionNumber].answers[1]}</span>
+    </label>
+    <label class="answer-option">
+    <input type="radio" value="${store[questionNumber].answers[2]}" name="answer" required>
+    <span>${store[questionNumber].answers[2]}</span>
+    </label>
+    <label class="answer-option">
+    <input type="radio" value="${store[questionNumber].answers[3]}" name="answer" required>
+    <span>${store[questionNumber].answers[3]}</span>
+    </label>
+    <button type="submit" class="submit-button">Submit</button>
+    </fieldset>
+    </form>
+    </div>`;
+} else {
+    renderResults();
+    handleRestartButton();
+    $('.question-number').text(10)
+  }
+}
+
+function renderResults() {
+  $('.col-12').remove();
+  if(score >=6) {
+    $('#quiz-box').html(`<div class='results correctFeedback'><h2>'Congrats, id say you know your stuff! '</h2><p>'You got ${score} / 10'</p><button class="restart-button">'Restart Quiz'</button></div>`);
+    }else if (score < 6) {
+      $('#quiz-box').html(`<div class="results correctFeedback"><h2>You didnt pass the test but hopefully you Learned along the way.</h2><p>You got ${score} / 10</p><button class="restart-button">Restart Quiz</button></div>`)
+    }
+}
+//increase score by 1
+function changeScore () {
+  score ++;
+}
+//render the question into the dom
+function renderNextQuestion() {
+  $('#quiz-box').html(generateQuestion());
+}
+//when user clicks start button
+//render the first question into the dom
+//set queston number to 1
+function startQuiz() {
+ $('.quiz-button').on('click', function() {
+   renderNextQuestion();
+   $('.question-number').text('1');
+
+ });
+}
+//when user presses submit button stop default action for button type submit
+//if user answer is correct return the feedback for being Right
+//if user is wrong return the feedback for being wrong
+function handleSubmitButton() {
+  $('#quiz-box').on('submit',  function (event) {
+    event.preventDefault();
+    
+    const selected = $('input:checked');
+    const answer = selected.val();
+    ;
+    if (answer === store[questionNumber].correctAnswer) {
+      ifAnswerIsCorrect();
+    } else {
+      ifAnswerIsWrong();
+    }
+  });
+}
+//update score increments the users score if their answer is correct
+function ifAnswerIsCorrect () {
+   correctFeedback();
+   updateScore();
+  
+}
+
+function ifAnswerIsWrong () {
+    wrongFeedback();
+    
+}
+
+function correctFeedback() {
+  $('#quiz-box').html(`
+    <section class="feedback-page">
+    <h2>You are right!</h2>
+    <img class='correct-img'src="https://media2.giphy.com/media/Yggr0uQUbA79C/giphy.gif" alt='Troy barnes and Abed Nadir'>
+    <button class="next-button">Next</button>
+    </section>
+  
+`);
+}
+
+function wrongFeedback() {
+  //const correctAnswer = `${store[questionNumber].correctAnswer}`;
+  $('#quiz-box').html(`
+  <section class="feedback-page"><h2>Incorrect it was ${store[questionNumber].correctAnswer}! </h2>
+    <img  src="https://media3.giphy.com/media/gQsfOW387X9Hq/giphy.gif" alt='Troy barnes of nbc show Community'>
+    <button class='next-button'>Next</button></section>`);
+}
+
+
+
+//update score text
+function updateScore () {
+  changeScore();
+  $('.score').text(score);
+}
+//when the user presses the next button
+//the quextion number should update and render the correct question
+function handleNextButton() {
+  $('#quiz-box').on('click', '.next-button', function() {
+    iterateQuestionNumber();
+    renderNextQuestion();  
+  });
+}
+
+function iterateQuestionNumber () {
+  if(questionNumber < 11) {
+    questionNumber ++;
+   }
+  console.log(questionNumber);
+  $('.question-number').text(questionNumber+1);
+}
+
+function handleRestartButton() {
+  $('#quiz-box').on('click', '.restart-button', function(event) {
+    location.reload();
+  });
+}
+
+function createQuiz () {
+  startQuiz();
+  handleSubmitButton();
+  handleNextButton();
+  handleRestartButton();
+}
+//when  the DOM is loaded run createQuiz function
+$(createQuiz);
